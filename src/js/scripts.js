09a6be3ecfeb3b17c6474727a75396ad95d66dea
Morsal Niyaz <morsalniyaz@gmail.com>
2023-06-09 15:51:32 -0700

    added products, gallery and updated mobile viewing

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -27,3 +27,27 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+// $(document).ready(function () {
+//     $(".owl-carousel").owlCarousel();
+// });
+
+// $('.owl-carousel').owlCarousel({
+//     items: 1,
+//     margin: 10,
+//     autoHeight: true
+// });
+
+
+$(document).ready(function () {
+    $("#news-slider").owlCarousel({
+        items: 3,
+        itemsDesktop: [1199, 3],
+        itemsDesktopSmall: [980, 2],
+        itemsMobile: [600, 1],
+        navigation: true,
+        navigationText: ["", ""],
+        pagination: true,
+        autoPlay: true
+    });
+});
